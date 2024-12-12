34be97e2eea6c3010c56adc9eedb67cb651f4086
Mirko <83219244+mirbyte@users.noreply.github.com>
2024-12-12 19:35:52 +0200

    Update installer.bat to v1.1

--- installer.bat
+++ installer.bat
@@ -54,30 +54,60 @@
  ::::::::::::::::::::::::::::
  ::START
  ::::::::::::::::::::::::::::
+ 
  REM github/mirbyte
- title VirusTotal Context Menu Installer (mirbyte)
+ 
+ title VirusTotal Context Menu Installer v1.1 (mirbyte)
+ setlocal enabledelayedexpansion
  cls
+ reg delete "HKEY_CLASSES_ROOT\*\shell\Scan with VirusTotal" /f >nul 2>&1
+ if %errorlevel% equ 0 (
+     echo Old scanner registry value was found.
+     echo.
+ ) else (
+     echo.
+ )
  echo Enter your VirusTotal API key:
  set /p api_key=""
  :: FIX SPACES
  for /f "delims=" %%a in ("%api_key%") do set api_key=%%a
  echo|set /p="%api_key%" > "files\api_key.txt"
-
+ 
+ cls
+ echo Please wait... This might take a minute or two.
+ 
+ type nul > files\api_k.txt
+ REM              😁
+ for /f "delims=" %%A in (files\api_key.txt) do (
+     set "X=%%A"
+     set "Y="
+    
+     for /l %%B in (0,1,255) do (
+         set "Z=!X:~%%B,1!"
+         if defined Z (
+             for /f "tokens=1 delims==" %%C in ('powershell -command "[int][char]'!Z!'"') do set "A=%%C"
+             set /a "B=A+1"
+             for /f "tokens=1 delims==" %%D in ('powershell -command "[char]!B!"') do set "F=%%D"
+             set "Y=!Y!!F!"
+         )
+     )
+     echo !Y! >> files\api_k.txt
+ )
  cls
  echo Do you want to install the program to %ProgramFiles%\VirusTotalScanner?
- set /p confirm="Press Y for Yes, N for No: "
+ set /p confirm="Press Y for yes, N for no: "
  cls
  if /i "%confirm%"=="Y" (
 	 if not exist "%ProgramFiles%\VirusTotalScanner" mkdir "%ProgramFiles%\VirusTotalScanner"
      copy /Y "files\vt_scanner.exe" "%ProgramFiles%\VirusTotalScanner\vt_scanner.exe"
-     copy /Y "files\api_key.txt" "%ProgramFiles%\VirusTotalScanner\api_key.txt"
+     copy /Y "files\api_k.txt" "%ProgramFiles%\VirusTotalScanner\api_k.txt"
 	 
      reg add "HKEY_CLASSES_ROOT\*\shell\Scan with VirusTotal" /ve /t REG_SZ /d "Scan with VirusTotal" /f
      reg add "HKEY_CLASSES_ROOT\*\shell\Scan with VirusTotal\command" /t REG_SZ /d "\"%ProgramFiles%\\VirusTotalScanner\\vt_scanner.exe\" \"%%1\"" /f
 	 
      echo.
      echo.
-     echo Installation completed.
+     echo Installation completed, please check for errors.
      pause
  ) else (
      echo.
