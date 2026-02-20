c0576d1053763f708ac52e9a8bcba65225e9015d
mirko <83219244+mirbyte@users.noreply.github.com>
2026-02-20 14:24:25 +0200

    Update uninstaller.bat

--- uninstaller.bat
+++ uninstaller.bat
@@ -1,39 +1,52 @@
 @echo off
+
 cd /d "%~dp0"
 
 net session >nul 2>&1
+
 if %errorlevel% neq 0 (
-    echo ERROR: Run this script as administrator.
-    echo Right-click the file and select "Run as administrator".
-    echo.
-    pause >nul
-    exit /b
+echo ERROR: Run this script as administrator.
+echo Right-click the file and select "Run as administrator".
+echo.
+pause >nul
+exit /b
 )
 
 REM github/mirbyte
-REM v1.1
 
 color 80
 title VTCM Uninstaller (mirbyte)
 setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+
 cls
 
-reg delete "HKEY_CLASSES_ROOT\*\shell\Scan with VirusTotal" /f >nul 2>&1
-if %errorlevel% equ 0 (
-    echo Registry entry removed successfully.
+set "REG_HKLM=HKLM\SOFTWARE\Classes\*\shell\Scan with VirusTotal"
+set "REG_HKCU=HKCU\Software\Classes\*\shell\Scan with VirusTotal"
+set "INSTDIR=%ProgramFiles%\VirusTotalScanner"
+
+set "reg_removed=0"
+
+reg delete "%REG_HKLM%" /f >nul 2>&1
+if %errorlevel% equ 0 set "reg_removed=1"
+
+reg delete "%REG_HKCU%" /f >nul 2>&1
+if %errorlevel% equ 0 set "reg_removed=1"
+
+if "%reg_removed%"=="1" (
+echo Registry entry removed successfully.
 ) else (
-    echo No previous registry entry found.
+echo No previous registry entry found.
 )
 
-if exist "%ProgramFiles%\VirusTotalScanner" (
-    powershell -Command "Remove-Item -Path '%ProgramFiles%\VirusTotalScanner' -Recurse -Force"
-    if exist "%ProgramFiles%\VirusTotalScanner" (
-        echo Failed to remove program files. Try closing any programs that may be using them.
-    ) else (
-        echo Program files removed successfully.
-    )
+if exist "%INSTDIR%" (
+powershell -Command "Remove-Item -Path '%INSTDIR%' -Recurse -Force"
+if exist "%INSTDIR%" (
+echo Failed to remove program files. Try closing any programs that may be using them.
 ) else (
-    echo No previous installation found.
+echo Program files removed successfully.
+)
+) else (
+echo No previous installation found.
 )
 
 echo.
